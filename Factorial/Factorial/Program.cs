3c011658b6dc391350f29ada5275611a64477d64
Himanshu Rawat <122114512+thehimanshurawat@users.noreply.github.com>
2025-02-14 12:01:09 +0530

    Day 2: Introduction to C# Basics
    Source Code For Factorial Program

---
+++ Factorial/Factorial/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int num = int.Parse(Console.ReadLine());
+            long fact=1;
+            for (int i = 1; i <= num; i++)
+            {
+                fact *= i;
+            }
+            Console.Write($"Factorial of a Entered number: {fact}");
+        }
+    }
+}
