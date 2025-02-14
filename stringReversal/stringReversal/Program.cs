b56802bfefe829616df6c198813a110a40667955
Himanshu Rawat <122114512+thehimanshurawat@users.noreply.github.com>
2025-02-14 12:03:56 +0530

    Day 2: Introduction to C# Basics
    Source code for string reversal program

---
+++ stringReversal/stringReversal/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    // Method to reverse a string using a loop
+    static string Reversed(string input)
+    {
+        string reversedString = "";
+
+        
+        for (int i = input.Length - 1; i >= 0; i--)
+        {
+            reversedString += input[i];
+        }
+
+        return reversedString;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a string: ");
+        string input = Console.ReadLine();
+
+        Console.WriteLine("Reversed String: " + Reversed(input));
+    }
+}
